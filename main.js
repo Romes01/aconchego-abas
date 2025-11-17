c579a7641ed7c468f9fb5b3985e149f055e41ec3
Fernando Romes <rodriguesnando159@gmail.com>
2025-11-17 19:22:48 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// Arquivo JavaScript principal para interatividade (ex: carrossel, tabela)
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Projeto Cafeteria Aconchego - Vanilla JS carregado.");
+
+    // Adicionar lógica de interatividade aqui, se necessário.
+});
